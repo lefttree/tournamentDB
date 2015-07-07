e0686e2c5dd39e151a889553d0ea757a1d58191d
xiang <bylixiang@gmail.com>
2015-07-06 23:53:21 -0700

    refactor: add some comments for tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -6,22 +6,38 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Drop database if there is already a tournament database
 DROP DATABASE IF EXISTS tournament;
+-- Create and connect to database
 CREATE DATABASE tournament;
 \c tournament;
 
-
+/*
+* Players table 
+* columns: id - player's id, name - player's name
+* purpose: represent each player
+*/
 CREATE TABLE Players (
 	id SERIAL primary key,
 	name varchar(255)
 );
 
+/*
+* Matches table
+* columns: id - match's id, winner - winner's player id, loser - loser's player id
+* purpose: represent each match's result
+*/
 CREATE TABLE Matches (
 	id SERIAL primary key,
 	winner int references players(id),
 	loser int references players(id)
 	);
 
+/*
+* Wins view
+* column: players.id, n - player's wins
+* purpose: count each player's number of wins
+*/
 CREATE VIEW Wins AS
 	SELECT Players.id, COUNT(Matches.winner) AS n
 	FROM Players
@@ -29,6 +45,11 @@ CREATE VIEW Wins AS
 	ON Players.id = Matches.winner
 	GROUP BY Players.id;
 
+/*
+* Count view
+* column: players.id, n - player's matches
+* purpose: count each player's number of matches
+*/
 CREATE VIEW Count AS
 	SELECT Players.id, COUNT(Matches.winner) AS n
 	FROM Players
@@ -36,6 +57,11 @@ CREATE VIEW Count AS
 	ON Players.id = Matches.winner OR Players.id = Matches.loser
 	GROUP BY Players.id;
 
+/*
+* Wins view
+* column: players.id, players.name, Wins.n, Count.n
+* purpose: combine each player's number of wins and matches
+*/
 CREATE VIEW Standings AS
 	SELECT Players.id, Players.name, Wins.n as wins,Count.n as matches
 	FROM Players, Count, Wins
